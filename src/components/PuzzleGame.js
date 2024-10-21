aa2352f7dadabf16a96e5b4253b7f1f7907d371c
Facumirande <facundomirande96@gmail.com>
2024-10-21 18:54:39 -0300

    update

---
+++ src/components/PuzzleGame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PuzzleGame.css';
+
+
+const PuzzleGame = () => {
+  return (
+    <div>
+      <h1>Puzzle Game</h1>
+      {/* Aquí iría el contenido del HTML que tenías */}
+    </div>
+  );
+};
+
+export default PuzzleGame;
